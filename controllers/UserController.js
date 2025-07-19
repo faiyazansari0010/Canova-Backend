7733625792ef906bd0ea2d869ede10681b6079a9
Faiyaz <faiyazansari0010@gmail.com>
2025-07-19 21:22:28 +0530

    Adding files

---
+++ controllers/UserController.js
@@ -0,0 +1,56 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { User } from "../models/User.js";
+
+const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(201).json({
+      message: "User created successfully",
+      user: {
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+      },
+    });
+
+    console.log(token)
+  } catch (error) {
+    console.error("Signup error:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+module.exports = {
+  // loginUser,
+  signup,
+};
--- server.js
+++ server.js
@@ -1,35 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { connectDB } from "./databse/connection.js";
+import UserRoutes from "./routes/UserRoutes.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-// Middleware
+connectDB();
+
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: "https://canova-frontend.netlify.app",
   credentials: true,
 }));
 app.use(express.json());
 app.use(cookieParser());
 
-// Routes
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'Backend is running ✅' });
-});
+app.use("/user", UserRoutes);
 
-// DB Connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('MongoDB connected');
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-  });
-}).catch((err) => {
-  console.error('MongoDB connection error:', err);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
